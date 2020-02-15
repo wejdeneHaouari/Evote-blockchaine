003709d2c5538c8aeb09177c7091c2fcc346e8b0
rached1997 <hawejdene@gmail.com>
2020-02-15 16:59:44 +0100

    add client side

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,26 @@ pragma solidity  0.5.16;
 
 contract Election {
 
-    string public candidate;
-    //Constructor
+    //Model a Candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //Store Candidates
+    //Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    //Store Candidate Count
+    uint public candidatesCount;
+
     constructor () public{
-        // Store candidate
-        candidate = "Candidate1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
