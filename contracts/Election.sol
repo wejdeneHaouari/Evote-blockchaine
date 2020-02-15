fe008258b1fb6a6817ecc23a1acc14110ae1ca18
rached1997 <hawejdene@gmail.com>
2020-02-15 15:27:25 +0100

    first contract

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity  0.5.16;
+
+contract Election {
+
+    string public candidate;
+    //Constructor
+    constructor () public{
+        // Store candidate
+        candidate = "Candidate1";
+    }
+}
